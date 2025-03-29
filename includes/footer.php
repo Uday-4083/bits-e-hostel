5072222b09f7430e777b295b5f7082c73e786eb5
Uday <udaygurrapu483@gmail.com>
2025-03-29 23:29:11 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer text-center text-muted" style="color: inherit;">
+    <div>
+        <a style="padding:10px" href="" target="_blank"><i class="fab fa-github"></i></a>
+        <a style="padding:10px" href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+        <a style="padding:10px" href="" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a style="padding:10px" href="" target="_blank"><i class="fa fa-h-square"></i></a>
+    </div>
+    <hr>
+    <div>
+        &copy; <?php echo date("Y"); ?> - BITS E-Hostel System - Developed by Gurrapu Uday Kumar & Team
+    </div>
+ 
+</footer>
